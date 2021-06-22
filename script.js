1f34e2e81a6b81a09f26a72f7b6f711c869d494a
Lapto-Vu <smallcounter@gmail.com>
2021-06-22 22:41:57 +0900

    1 스택 큐 - 2번 문제

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-// function solution(participant, completion) {
-//     completion.forEach(x => {
-//         const idx = participant.indexOf(x)
-//         if (idx !== null) {
-//             participant.splice(idx, 1)
-//         }
-//     })
-//     const [ answer ] = participant
-//     return answer
-// }
-
-function solution(part, comp) {
-    let answer = []
-    let i = 0
-    part.sort()
-    comp.sort()
-
-    while ( i < part.length) {
-        if (part[i] !== comp[i]) {
-            answer.push(part[i])
+function solution(progresses, speeds) {
+    var answer = [];
+    var portion = [];
+    let currentBox = 0;
+    let tag = -1;
+    
+    for(let i=0; i < progresses.length; i++ ) {
+        for (let k=1; progresses[i] < 100; k++ ) {
+            progresses[i] += speeds[i];
+            portion[i] = k;
         }
-        i++
+
+        if(currentBox < portion[i]) {
+            tag++
+            currentBox=portion[i]
+            answer[tag] = 1
+        } else {
+            answer[tag]++
+        }
+
     }
-    const [result] = answer
-    return result
+
+    return answer;
 }
 
-const part = ["marina", "josipa", "nikola", "vinko", "filipa"]
-const comp = ["josipa", "filipa", "marina", "nikola"]
-const result = solution(part, comp)
+const prog = [93, 30, 55]
+const speed  = [1, 30, 5]
+const result = solution(prog, speed)
 console.log(result)
