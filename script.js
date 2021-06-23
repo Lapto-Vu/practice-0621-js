9b62e4ac5e4070a2a23594d53caad097fde1b712
Lapto-Vu <smallcounter@gmail.com>
2021-06-23 15:45:03 +0900

    Minor-Updated

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
-function solution(progresses, speeds) {
-    var answer = [];
-    var portion = [];
-    let currentBox = 0;
-    let tag = -1;
-    
-    for(let i=0; i < progresses.length; i++ ) {
-        for (let k=1; progresses[i] < 100; k++ ) {
-            progresses[i] += speeds[i];
-            portion[i] = k;
+function solution(clothes) {
+    let portion = [{ value: "zero", key: 0 }];
+    let newSet = { value: null, key: 1 }
+    let tag = true
+
+    for (let i = 0; i < clothes.length; i++) {
+
+        for (let s = 0; s < portion.length; s++) {
+            if (portion[s].value === clothes[i][1]) {
+                portion[s].key++;
+                tag = false
+            }
         }
 
-        if(currentBox < portion[i]) {
-            tag++
-            currentBox=portion[i]
-            answer[tag] = 1
-        } else {
-            answer[tag]++
+        if (tag) {
+            const text = clothes[i][1]
+            newSet = {
+                value: text,
+                key: 1
+            }
+            portion.push(newSet);
+        }
+
+        tag = true
+    }
+
+    portion.shift()
+
+    console.log(portion)
+
+    let sort = 0;
+    let sum = (2 * portion.length) - 1
+    let token = 2 ** (portion.length - 1)
+    console.log(token)
+
+    for (let p = 0; p < portion.length; p++) {
+        if (portion[p].key > 1) {
+            sum += token * (portion[p].key - 1);
+            sort = portion[p].key
+            console.log(sum)
+        }
+
+        if (sort < portion[p].key) {
+            console.log("ns")
+            token++
+            sum += token;
         }
 
     }
 
-    return answer;
+    return sum
 }
 
-const prog = [93, 30, 55]
-const speed  = [1, 30, 5]
-const result = solution(prog, speed)
+
+const clothes = [["yellowhat", "headgear"], ["crowmask", "face"], ["bluesunglasses", "face"], ["green_turban", "headgear"]]
+const result = solution(clothes);
 console.log(result)
